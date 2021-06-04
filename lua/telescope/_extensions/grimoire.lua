2522a3240d51593c69d43fff08503cbdac4f6d0c
Alan W. Smith <alan@alanwsmith.com>
2021-06-03 22:36:26 -0400

    Testing stuff

--- lua/telescope/_extensions/grimoire.lua
+++ lua/telescope/_extensions/grimoire.lua
@@ -1,11 +1,19 @@
 
-local file_code_actions = function(opts)
-    mw = 'asdf'
+local commands = function(opts)
+  pickers.new(opts, {
+    prompt_title = 'ConfigPicker',
+    finder = finders.new_table {
+      results = {
+         "path/to/file"
+      },
+      entry_maker = opts.entry_maker,
+    },
+  }):find()
 end
 
 return require('telescope').register_extension{
   exports = {
-    file_code_actions = file_code_actions 
+    commands = commands 
   },
 }
 
