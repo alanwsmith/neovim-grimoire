2921a3107439027d4c61d1a1840ea5d46f246985
Alan W. Smith <alan@alanwsmith.com>
2021-06-06 17:19:19 -0400

    Getting and sending the data to itself

--- lua/grimoire.lua
+++ lua/grimoire.lua
@@ -3,9 +3,9 @@ local sbuf, rbuf
 local function open_search_window()
     sbuf = vim.api.nvim_create_buf(false, true)
     swin = vim.api.nvim_open_win(sbuf, true ,
-            {style = "minimal",relative='win', row=5, col=5, width=40, height=4}
+            {style = "minimal",relative='win', row=5, col=5, width=55, height=6}
         )
-    vim.api.nvim_command('au CursorMoved <buffer> lua require"grimoire".show_results()')
+    vim.api.nvim_command('au CursorMoved,CursorMovedI <buffer> lua require"grimoire".show_results()')
 end
 
 local function open_results_window()
@@ -16,7 +16,9 @@ local function open_results_window()
 end
 
 local function show_results()
-    vim.api.nvim_command(':lua print("BEEEER")')
+    -- vim.api.nvim_command(':lua print("BEEEER")')
+    local lines = vim.api.nvim_buf_get_lines(0, 0, 1, false)
+    vim.api.nvim_buf_set_lines(0, 2, 3, false, lines)
 end
 
 local function grimoire()
