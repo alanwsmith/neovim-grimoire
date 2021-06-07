4a6bd71ebcadf4fbbc579a8350a78b2f7ffd85c0
Alan W. Smith <alan@alanwsmith.com>
2021-06-06 20:23:17 -0400

    Calling data properly to feed the respose windows

--- lua/grimoire.lua
+++ lua/grimoire.lua
@@ -1,5 +1,6 @@
 local sbuf, rbuf
 
+
 local function open_search_window()
     sbuf = vim.api.nvim_create_buf(false, true)
     swin = vim.api.nvim_open_win(sbuf, true ,
@@ -17,8 +18,12 @@ end
 
 local function show_results()
     -- vim.api.nvim_command(':lua print("BEEEER")')
-    local lines = vim.api.nvim_buf_get_lines(0, 0, 1, false)
-    vim.api.nvim_buf_set_lines(0, 2, 3, false, lines)
+    -- local lines = vim.api.nvim_buf_get_lines(0, 0, 1, false)
+    -- local lines = vim.fn.systemlist('curl -s https://www.example.com/') 
+    local lines = vim.fn.systemlist('curl -s "http://127.0.0.1:7700/indexes/grimoire/search?q=py" | jq -r ".hits[] | .name"')
+    -- local json_data = vim.json_decode(vim.fn.systemlist('curl -s "http://127.0.0.1:7700/indexes/grimoire/search?q=py"'))
+    -- local json_data = vim.fn.json_decode('{"asdf": "erere"}') 
+    vim.api.nvim_buf_set_lines(rbuf, 3, 3, false, lines)
 end
 
 local function grimoire()
