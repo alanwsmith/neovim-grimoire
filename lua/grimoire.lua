5d7dfa533bc9e993e08c4d2219d253b551f24283
Alan W. Smith <alan@alanwsmith.com>
2021-06-06 20:34:41 -0400

    Basic search and output of pages to open is in place

--- lua/grimoire.lua
+++ lua/grimoire.lua
@@ -1,10 +1,9 @@
 local sbuf, rbuf
 
-
 local function open_search_window()
     sbuf = vim.api.nvim_create_buf(false, true)
     swin = vim.api.nvim_open_win(sbuf, true ,
-            {style = "minimal",relative='win', row=5, col=5, width=55, height=6}
+            {style = "minimal",relative='win', row=5, col=5, width=55, height=1}
         )
     vim.api.nvim_command('au CursorMoved,CursorMovedI <buffer> lua require"grimoire".show_results()')
 end
@@ -12,18 +11,15 @@ end
 local function open_results_window()
   rbuf = vim.api.nvim_create_buf(false, true)
   vim.api.nvim_open_win(rbuf, false,
-        {style = "minimal",relative='win', row=20, col=5, width=40, height=4}
+        {style = "minimal",relative='win', row=10, col=5, width=40, height=12}
     )
 end
 
 local function show_results()
-    -- vim.api.nvim_command(':lua print("BEEEER")')
-    -- local lines = vim.api.nvim_buf_get_lines(0, 0, 1, false)
-    -- local lines = vim.fn.systemlist('curl -s https://www.example.com/') 
-    local lines = vim.fn.systemlist('curl -s "http://127.0.0.1:7700/indexes/grimoire/search?q=py" | jq -r ".hits[] | .name"')
-    -- local json_data = vim.json_decode(vim.fn.systemlist('curl -s "http://127.0.0.1:7700/indexes/grimoire/search?q=py"'))
-    -- local json_data = vim.fn.json_decode('{"asdf": "erere"}') 
-    vim.api.nvim_buf_set_lines(rbuf, 3, 3, false, lines)
+    local query = vim.api.nvim_buf_get_lines(0, 0, 1, false)
+    print(query)
+    local lines = vim.fn.systemlist('curl -s "http://127.0.0.1:7700/indexes/grimoire/search?q='..query[1]..'" | jq -r ".hits[] | .name"')
+    vim.api.nvim_buf_set_lines(rbuf, 0, 3, false, lines)
 end
 
 local function grimoire()
