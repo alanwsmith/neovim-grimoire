c9fbeeeb7d117dcc9ad9383b3ce2087b561c789f
Alan W. Smith <alan@alanwsmith.com>
2021-06-06 23:21:41 -0400

    Setup an initial highligh update that still needs some work

--- lua/grimoire.lua
+++ lua/grimoire.lua
@@ -1,14 +1,17 @@
 local rbuf, rwin
 local sbuf, swin
 
-
 local function open_search_window()
     sbuf = vim.api.nvim_create_buf(false, true)
     swin = vim.api.nvim_open_win(sbuf, true ,
             {style = "minimal",relative='win', row=5, col=5, width=55, height=1}
         )
     vim.api.nvim_command('au CursorMoved,CursorMovedI <buffer> lua require"grimoire".show_results()')
-    vim.api.nvim_buf_set_keymap(sbuf, 'i', '0', '<ESC> | :lua require"grimoire".change_selected_results() | i', {
+    -- vim.api.nvim_buf_set_keymap(sbuf, 'i', '0', '<ESC> | :lua require"grimoire".change_selected_results() | i', {
+    -- vim.api.nvim_buf_set_keymap(sbuf, 'i', '0', '<ESC> :lua require"grimoire".change_selected_results()<cr> ', {
+    -- vim.api.nvim_buf_set_keymap(sbuf, 'i', '0', require"grimoire".change_selected_results(), {
+    -- vim.api.nvim_buf_set_keymap(sbuf, 'i', '0', '<ESC> :lua require"grimoire".change_selected_results()<cr>', {
+    vim.api.nvim_buf_set_keymap(sbuf, 'i', '0', '<ESC>:lua require"grimoire".change_selected_results()<cr>', {
         nowait = true, 
         noremap = true, 
         silent = true
@@ -32,9 +35,9 @@ local function show_results()
 end
 
 local function change_selected_results()
-  local new_pos = api.nvim_win_get_cursor(rwin)[1] + 1
-  vim.api.nvim_win_set_cursor(rwin, {4, 0})
-  vim.api.nvim_buf_add_highlight(rbuf, -1, 'WhidHeader', 0, 0, -1)
+    -- local new_pos = api.nvim_win_get_cursor(rwin)[1] + 1
+    -- vim.api.nvim_win_set_cursor(rwin, {4, 0})
+    vim.api.nvim_buf_add_highlight(rbuf, -1, 'GrimoireSelection', 4, 0, -1)
 end
 
 local function grimoire()
@@ -47,6 +50,7 @@ end
 
 return {
   grimoire = grimoire,
+  change_selected_results = change_selected_results,
   show_results = show_results 
 }
 
