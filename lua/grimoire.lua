a2ca90bc082e6c2c7ed67a49340b19bf5718c5b1
Alan W. Smith <alan@alanwsmith.com>
2021-06-07 22:31:48 -0400

    Setup so that we have the ability to do a command from insert mode

--- lua/grimoire.lua
+++ lua/grimoire.lua
@@ -2,13 +2,18 @@ local rbuf, rwin
 local sbuf, swin
 local highlight_index = 0
 
+local function close_windows()
+    vim.api.nvim_win_close(rwin, true)
+    vim.api.nvim_win_close(swin, true)
+end
+
 local function open_search_window()
     sbuf = vim.api.nvim_create_buf(false, true)
     swin = vim.api.nvim_open_win(sbuf, true ,
             {style = "minimal",relative='win', row=5, col=5, width=55, height=1}
         )
     vim.api.nvim_command('au CursorMoved,CursorMovedI <buffer> lua require"grimoire".show_results()')
-    vim.api.nvim_buf_set_keymap(sbuf, 'i', '0', '<ESC>:lua require"grimoire".change_selected_results()<CR>a', {
+    vim.api.nvim_buf_set_keymap(sbuf, 'i', '[', '<cmd>lua require"grimoire".change_selected_results()<CR>', {
         nowait = true, 
         noremap = true, 
         silent = true
@@ -29,7 +34,7 @@ local function show_results()
     local query_string2 = string.gsub(query_string, '%s', '%%20')
     local lines = vim.fn.systemlist('curl -s "http://127.0.0.1:7700/indexes/grimoire/search?q='..query_string2..'&limit=8" | jq -r ".hits[] | .name"')
     vim.api.nvim_buf_set_lines(rbuf, 0, 9, false, lines)
-    vim.api.nvim_buf_add_highlight(rbuf, -1, 'GrimoireSelection', 1, 0, -1)
+    vim.api.nvim_buf_add_highlight(rbuf, -1, 'GrimoireSelection', highlight_index, 0, -1)
 end
 
 local function change_selected_results()
@@ -41,6 +46,7 @@ end
 local function grimoire()
     open_results_window()
     open_search_window()
+    vim.api.nvim_buf_set_keymap(sbuf, 'n', 'q', ':lua require("grimoire").close_windows()<CR>', {})
 end
 
 local function make_mappings()
@@ -49,6 +55,7 @@ end
 return {
   grimoire = grimoire,
   change_selected_results = change_selected_results,
+  close_windows = close_windows,
   show_results = show_results 
 }
 
