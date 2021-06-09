ad2382d76ad024e77fb3f29ce55f18930abe9574
Alan W. Smith <alan@alanwsmith.com>
2021-06-08 21:07:39 -0400

    Add stoppers for min and max result indexes

--- lua/grimoire.lua
+++ lua/grimoire.lua
@@ -3,6 +3,7 @@ local sbuf, swin
 local spacer_buf, spacer_win
 local selected_file_index = 0
 local highlight_namespace
+local result_list_length = 12
 
 local function close_windows()
     vim.api.nvim_win_close(spacer_win, true)
@@ -40,26 +41,29 @@ local function open_search_window()
     })
 
     vim.cmd('startinsert')
-
 end
 
 
 local function select_previous_index()
-    selected_file_index = selected_file_index - 1
-    vim.api.nvim_buf_clear_namespace(rbuf, -1, 0, -1)
-    vim.api.nvim_buf_add_highlight(rbuf, -1, 'GrimoireSelection', selected_file_index, 0, -1)
+    if selected_file_index > 0 then
+        selected_file_index = selected_file_index - 1
+        vim.api.nvim_buf_clear_namespace(rbuf, -1, 0, -1)
+        vim.api.nvim_buf_add_highlight(rbuf, -1, 'GrimoireSelection', selected_file_index, 0, -1)
+    end
 end
 
 local function select_next_index()
-    selected_file_index = selected_file_index + 1
-    vim.api.nvim_buf_clear_namespace(rbuf, -1, 0, -1)
-    vim.api.nvim_buf_add_highlight(rbuf, -1, 'GrimoireSelection', selected_file_index, 0, -1)
+    if selected_file_index < (result_list_length - 1) then
+        selected_file_index = selected_file_index + 1
+        vim.api.nvim_buf_clear_namespace(rbuf, -1, 0, -1)
+        vim.api.nvim_buf_add_highlight(rbuf, -1, 'GrimoireSelection', selected_file_index, 0, -1)
+    end
 end
 
 local function open_results_window()
   rbuf = vim.api.nvim_create_buf(false, true)
   rwin = vim.api.nvim_open_win(rbuf, false,
-        {style = "minimal",relative='win', row=7, col=4, width=55, height=12}
+        {style = "minimal",relative='win', row=7, col=4, width=55, height=result_list_length}
     )
 end
 
@@ -76,7 +80,7 @@ local function show_results()
     local query = vim.api.nvim_buf_get_lines(0, 0, 1, false)
     local query_string = string.gsub(query[1], '%s*$', '')
     local query_string2 = string.gsub(query_string, '%s', '%%20')
-    local lines = vim.fn.systemlist('curl -s "http://127.0.0.1:7700/indexes/grimoire/search?q='..query_string2..'&limit=8" | jq -r ".hits[] | .name"')
+    local lines = vim.fn.systemlist('curl -s "http://127.0.0.1:7700/indexes/grimoire/search?q='..query_string2..'&limit='..result_list_length..'" | jq -r ".hits[] | .name"')
     vim.api.nvim_buf_set_lines(rbuf, 0, 9, false, lines)
     highlight_namespace = vim.api.nvim_buf_add_highlight(rbuf, -1, 'GrimoireSelection', selected_file_index, 0, -1)
 end
